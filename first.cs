dbc60433702004ed6cf01e6da64ddbc124a8b38c
Babi Thakur <astra@localhost.localdomain>
2024-07-04 15:37:21 +0545

    Initial Commit

---
+++ first.cs
@@ -0,0 +1,8 @@
+using System; //importing library (package)
+namespace dotnet_tech{ //logical project folder name
+    internal class Program{ //default access modifier is "internal", Program ko satta je ni lekhna milxa
+        static void Main (String[] args){
+            Console.WriteLine("Hello from Astra!"); //its basically System.Console.WriteLine()
+        }
+    }
+}
